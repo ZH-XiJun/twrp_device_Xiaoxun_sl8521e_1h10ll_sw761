c2cc35d00297f01e5ced129be8a107d73b85c1a1
xunmod <116288337+xunmod@users.noreply.github.com>
2024-07-03 11:04:40 +0800

    Fix build; change usb

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,3 @@
+grep property_set bootable/recovery/minadbd21/usb_linux_client.c &>/dev/null || patch -f -p1 < device/Xiaoxun/sl8521e_1h10ll_sw761/sys_usb_ffs_ready.patch
 add_lunch_combo omni_sl8521e_1h10ll_sw761-eng
 add_lunch_combo omni_sl8521e_1h10ll_sw761-userdebug
